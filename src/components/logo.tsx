cdbbd5e51358c62ec562d1260142b44d91014349
Rugved Narkar <narkarrugved241@gmail.com>
2025-09-16 17:11:44 +0000

    Give blue colour to this text (_for element span_)

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -7,7 +7,7 @@ export function Logo({ className, showText = true }: { className?: string; showT
       <div className="bg-primary text-primary-foreground p-1.5 rounded-md">
         <Image src="https://storage.googleapis.com/aip-dev-images-public/console-user-images/user-43403310-8637-4f8a-9244-dfc5453d105b/f35136a7-02a8-4e00-9a64-0545050f2882.png" alt="logo" width={20} height={20} data-ai-hint="logo abstract" />
       </div>
-      {showText && <span className="text-xl tracking-tight text-white">SAMUDRA</span>}
+      {showText && <span className="text-xl tracking-tight text-primary">SAMUDRA</span>}
     </div>
   );
 }
