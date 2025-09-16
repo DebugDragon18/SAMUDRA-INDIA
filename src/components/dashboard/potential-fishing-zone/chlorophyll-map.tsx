028584dabde1e10f699ad5fab384f06f2905eea0
Rugved Narkar <narkarrugved241@gmail.com>
2025-09-16 18:10:40 +0000

    Ensure only one <MapContainer> instance              <MapContainer   key

--- src/components/dashboard/potential-fishing-zone/chlorophyll-map.tsx
+++ src/components/dashboard/potential-fishing-zone/chlorophyll-map.tsx
@@ -4,7 +4,7 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/com
 import { Skeleton } from "@/components/ui/skeleton";
 import dynamic from "next/dynamic";
 
-const MapContainer = dynamic(() => import('react-leaflet').then(mod => mod.MapContainer), { ssr: false });
+const MapContainer = dynamic(() => import('react-leaflet').then(mod => mod.MapContainer), { ssr: false, loading: () => <Skeleton className="h-[600px] w-full" /> });
 const TileLayer = dynamic(() => import('react-leaflet').then(mod => mod.TileLayer), { ssr: false });
 const WMSTileLayer = dynamic(() => import('react-leaflet').then(mod => mod.WMSTileLayer), { ssr: false });
 
@@ -18,20 +18,22 @@ export function ChlorophyllMap() {
             </CardHeader>
             <CardContent>
                 <div className="h-[600px] w-full">
-                <MapContainer center={[15, 80]} zoom={5} scrollWheelZoom={true} className="h-full w-full rounded-lg border">
-                    <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    />
-                    <WMSTileLayer
-                        url="/erddap/wms/apdrc_chlora_1d/request"
-                        layers="apdrc_chlora_1d:chlor_a"
-                        format="image/png"
-                        transparent={true}
-                        opacity={0.7}
-                        attribution="INCOIS ERDDAP"
-                    />
-                </MapContainer>
+                {typeof window !== 'undefined' && (
+                    <MapContainer center={[15, 80]} zoom={5} scrollWheelZoom={true} className="h-full w-full rounded-lg border">
+                        <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                        <WMSTileLayer
+                            url="/erddap/wms/apdrc_chlora_1d/request"
+                            layers="apdrc_chlora_1d:chlor_a"
+                            format="image/png"
+                            transparent={true}
+                            opacity={0.7}
+                            attribution="INCOIS ERDDAP"
+                        />
+                    </MapContainer>
+                )}
                 </div>
             </CardContent>
         </Card>
