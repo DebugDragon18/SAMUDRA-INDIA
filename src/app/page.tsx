48f9bf087de980bbc8a6de1461d3e839e65d4b51
Rugved Narkar <narkarrugved241@gmail.com>
2025-09-16 19:05:33 +0000

    Remove this (_for element img_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { AppHeader } from '@/components/layout/app-header';
 import { HeroSection } from '@/components/home/hero-section';
 import { DataPlatformSection } from '@/components/home/data-platform-section';
-import { FeaturesSection } from '@/components/home/features-section';
 import { CtaSection } from '@/components/home/cta-section';
 import { Footer } from '@/components/layout/footer';
 import { TeamSection } from '@/components/about/team-section';
@@ -16,7 +15,6 @@ export default function HomePage() {
       <main className="flex-1">
         <HeroSection />
         <DataPlatformSection />
-        <FeaturesSection />
         <KeyFeaturesSection />
         <CtaSection />
         <LatestNewsSection />
