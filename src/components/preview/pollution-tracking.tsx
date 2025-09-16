947594fb3edfde997d352cf909cb726261a66e43
Rugved Narkar <narkarrugved241@gmail.com>
2025-09-16 10:07:48 +0000

    Add Video under Earth Ocean Salinity Map. ( video Link :- https://docs.g

--- src/components/preview/pollution-tracking.tsx
+++ src/components/preview/pollution-tracking.tsx
@@ -9,7 +9,7 @@ import {
 export function PollutionTracking() {
   return (
     <section className="bg-white/5 py-16 md:py-24">
-      <div className="container mx-auto">
+      <div className="container mx-auto space-y-8">
         <Card className="shadow-lg">
           <CardHeader>
             <CardTitle>Earth Ocean Salinity Map</CardTitle>
@@ -23,6 +23,24 @@ export function PollutionTracking() {
                 src="https://svs.gsfc.nasa.gov/vis/a010000/a011200/a011211/index.html"
                 className="h-full w-full rounded-lg border"
                 title="Earth Ocean Salinity Map"
+                allowFullScreen
+              />
+            </div>
+          </CardContent>
+        </Card>
+        <Card className="shadow-lg">
+          <CardHeader>
+            <CardTitle>Ocean Surface Currents Visualization</CardTitle>
+            <CardDescription>
+              A video visualizing ocean surface currents.
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="relative h-[600px] w-full">
+              <iframe
+                src="https://drive.google.com/file/d/1n0NbAEmP05ik_d9toJdoInrNmo6Gktas/preview"
+                className="h-full w-full rounded-lg border"
+                title="Ocean Surface Currents Visualization"
                 allowFullScreen
               />
             </div>
