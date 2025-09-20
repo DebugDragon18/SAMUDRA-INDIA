a923be554be4031788348f85c892be1531396fe9
Rugved Narkar <narkarrugved241@gmail.com>
2025-09-20 09:34:01 +0000

    Please update the image here to the newly chosen ones (_for element head

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,7 +4,7 @@ import { cn } from '@/lib/utils';
 export function Logo({ className, showText = true }: { className?: string; showText?: boolean }) {
   return (
     <div className={cn("flex items-center gap-3 font-bold text-primary", className)}>
-      <Image src="https://storage.googleapis.com/aip-dev-images-public/console-user-images/user-43403310-8637-4f8a-9244-dfc5453d105b/61f22143-4b69-4e4b-84a1-026f39e55132.png" alt="logo" width={40} height={40} data-ai-hint="logo abstract" />
+      <Image src="https://images.unsplash.com/photo-1559825481-12a05cc00344?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwzfHxvY2VhbiUyMHxlbnwwfHx8fDE3NTgzNjA4MTh8MA&ixlib=rb-4.1.0&q=80&w=1080" alt="logo" width={40} height={40} data-ai-hint="logo abstract" className="rounded-full" />
       {showText && (
         <div className="flex flex-col">
             <span className="text-2xl tracking-tight font-bold text-primary">SAMUDRA</span>
